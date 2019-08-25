a906bcfbb2f2ae1c7cddb717978232aae9dc7929
Guillermo Ruffino <glm.net@gmail.com>
2019-08-25 16:58:58 -0300

    APE recover if arduino reboots

--- ape.h
+++ ape.h
@@ -24,38 +24,43 @@ static const char *TAGape = "ape";
 #define ape_binary_sensor(ape, pin) get_ape(ape)->get_binary_sensor(pin)
 //#define ape_sensor(ape, pin) get_ape(ape)->get_sensor(pin)
 
+
+
 class ArduinoPortExtender;
 
 using namespace esphome;
 
 class ApeBinaryOutput : public output::BinaryOutput
 {
-  public:
-    ApeBinaryOutput(ArduinoPortExtender *parent, uint8_t pin)
-    {
-        this->parent_ = parent;
-        this->pin_ = pin;
-    }
-    void write_state(bool state) override;
-    uint8_t get_pin() { return this->pin_; }
+public:
+  ApeBinaryOutput(ArduinoPortExtender *parent, uint8_t pin)
+  {
+    this->parent_ = parent;
+    this->pin_ = pin;
+  }
+  void write_state(bool state) override;
+  uint8_t get_pin() { return this->pin_; }
 
-  protected:
-    ArduinoPortExtender *parent_;
-    uint8_t setup_{true};
-    uint8_t pin_;
+protected:
+  ArduinoPortExtender *parent_;
+  uint8_t pin_;
+  bool state_{false};
+
+  friend class ArduinoPortExtender;
 };
+
 
 class ApeBinarySensor : public binary_sensor::BinarySensor
 {
-  public:
-    ApeBinarySensor(ArduinoPortExtender *parent, uint8_t pin)
-    {
-        this->pin_ = pin;
-    }
-    uint8_t get_pin() { return this->pin_; }
+public:
+  ApeBinarySensor(ArduinoPortExtender *parent, uint8_t pin)
+  {
+    this->pin_ = pin;
+  }
+  uint8_t get_pin() { return this->pin_; }
 
-  protected:
-    uint8_t pin_;
+protected:
+  uint8_t pin_;
 };
 /*
 class ApeSensor : public sensor::Sensor
@@ -73,120 +78,152 @@ class ApeSensor : public sensor::Sensor
 
 class ArduinoPortExtender : public Component, public I2CDevice
 {
-  public:
-    ArduinoPortExtender(I2CComponent *parent, uint8_t address) : I2CDevice(parent, address)
-    {
-    }
-    void setup() override
-    {
-        #ifdef APE_DEBUG
-        ESP_LOGCONFIG(TAGape, "Setting up ArduinoPortExtender at %d ... waiting up to 3 secs", address_);
-        #endif
+public:
+  ArduinoPortExtender(I2CComponent *parent, uint8_t address) : I2CDevice(parent, address)
+  {
+  }
+  void setup() override
+  {
+#ifdef APE_DEBUG
+    ESP_LOGCONFIG(TAGape, "Setting up ArduinoPortExtender at %#02x ...", address_);
+#endif
 
-        /* We cannot setup as usual as arduino boots later than esp8266
+    /* We cannot setup as usual as arduino boots later than esp8266
 
             Poll i2c bus for our Arduino for a n seconds instead of failing fast,
             also this is important as pin setup (INPUT_PULLUP, OUTPUT it's done once)
         */
-
-        unsigned long start = millis();
-
-        while (millis() - start < 5000) // Usually is alive before 200 milliseconds
-        {
-            if (this->read_bytes(APE_CMD_DIGITAL_READ, const_cast<uint8_t *>(this->read_buffer_), 3, 1))
-            {
-                #ifdef APE_DEBUG
-                ESP_LOGCONFIG(TAGape, "ArduinoPortExpander found at %d in %d millis", address_, millis() - start);
-                #endif
-                delay(20);
-
-                for (ApeBinarySensor *pin : this->input_pins_)
-                {
-                    uint8_t pinNo = pin->get_pin();
-                    #ifdef APE_DEBUG
-                    ESP_LOGCONFIG(TAGape, "Setup input pin %d", pinNo);
-                    #endif
-                    this->write_byte(APE_CMD_SETUP_PIN_INPUT_PULLUP, pinNo);
-                    delay(20);
-                }
-
-                return;
-            }
-            delay(20);
-        }
-        #ifdef APE_DEBUG
-        ESP_LOGE(TAGape, "ArduinoPortExpander NOT found at %d in %d millis", address_, millis() - start);
-        #endif
-        this->mark_failed();
-    }
-    void loop() override
+    this->configure_timeout_ = millis() + 5000;
+  }
+  void loop() override
+  {
+    if (millis() < this->configure_timeout_)
     {
-        this->read_bytes(APE_CMD_DIGITAL_READ, const_cast<uint8_t *>(this->read_buffer_), 3, 1);
+      bool try_configure = millis() % 100 > 50;
+      if (try_configure == this->configure_)
+        return;
+      this->configure_ = try_configure;
+
+      if (this->read_bytes(APE_CMD_DIGITAL_READ, const_cast<uint8_t *>(this->read_buffer_), 3, 1))
+      {
+#ifdef APE_DEBUG
+        ESP_LOGCONFIG(TAGape, "ArduinoPortExpander found at %#02x", address_);
+#endif
+        delay(10);
+
+        // Config success
+        this->configure_timeout_ = 0;
+        this->status_clear_error();
+
         for (ApeBinarySensor *pin : this->input_pins_)
         {
-            uint8_t pinNo = pin->get_pin();
-
-            uint8_t bit = pinNo % 8;
-            uint8_t value = pinNo < 8 ? this->read_buffer_[0] : pinNo < 16 ? this->read_buffer_[1] : this->read_buffer_[2];
-            bool ret = value & (1 << bit);
-            if (this->initial_state_)
-                pin->publish_initial_state(ret);
-            else
-                pin->publish_state(ret);
+          App.feed_wdt();
+          uint8_t pinNo = pin->get_pin();
+#ifdef APE_DEBUG
+          ESP_LOGCONFIG(TAGape, "Setup input pin %d", pinNo);
+#endif
+          this->write_byte(APE_CMD_SETUP_PIN_INPUT_PULLUP, pinNo);
+          delay(20);
         }
-        this->initial_state_ = false;
+        for (ApeBinaryOutput *output : this->output_pins_)
+        {
+          App.feed_wdt();
+          this->write_state(output->pin_, output->state_, true);
+          delay(20);
+        }
+        return;
+      }
+      // Still not answering
+      return;
+    }
+    if (this->configure_timeout_ != 0 && millis() > this->configure_timeout_)
+    {
+#ifdef APE_DEBUG
+      ESP_LOGE(TAGape, "ArduinoPortExpander NOT found at %#02x", address_);
+#endif
+      this->mark_failed();
+      return;
     }
 
- /*   uint16_t analogRead(uint8_t pin)
+    if (!this->read_bytes(APE_CMD_DIGITAL_READ, const_cast<uint8_t *>(this->read_buffer_), 3, 1))
+    {
+      ESP_LOGE(TAGape, "Error reading. Reconfiguring pending.");
+      this->status_set_error();
+      this->configure_timeout_ = millis() + 5000;
+      return;
+    }
+    for (ApeBinarySensor *pin : this->input_pins_)
+    {
+      uint8_t pinNo = pin->get_pin();
+
+      uint8_t bit = pinNo % 8;
+      uint8_t value = pinNo < 8 ? this->read_buffer_[0] : pinNo < 16 ? this->read_buffer_[1] : this->read_buffer_[2];
+      bool ret = value & (1 << bit);
+      if (this->initial_state_)
+        pin->publish_initial_state(ret);
+      else
+        pin->publish_state(ret);
+    }
+    this->initial_state_ = false;
+  }
+
+  /*   uint16_t analogRead(uint8_t pin)
     {
         this->read_bytes((uint8_t)(CMD_ANALOG_READ_A0 + pin), const_cast<uint8_t *>(this->read_buffer_), 2, 1);
         uint16_t value = this->read_buffer_[0] | ((uint16_t)this->read_buffer_[1] << 8);
         return value;
     }*/
 
-    output::BinaryOutput *get_binary_output(uint8_t pin)
-    {
-        ApeBinaryOutput *output = new ApeBinaryOutput(this, pin);
-        output_pins_.push_back(output);
-        return output;
-    }
-    binary_sensor::BinarySensor *get_binary_sensor(uint8_t pin)
-    {
-        ApeBinarySensor *binarySensor = new ApeBinarySensor(this, pin);
-        input_pins_.push_back(binarySensor);
-        return binarySensor;
-    }
-/*    sensor::Sensor *get_sensor(uint8_t pin)
+  output::BinaryOutput *
+  get_binary_output(uint8_t pin)
+  {
+    ApeBinaryOutput *output = new ApeBinaryOutput(this, pin);
+    output_pins_.push_back(output);
+    return output;
+  }
+  binary_sensor::BinarySensor *get_binary_sensor(uint8_t pin)
+  {
+    ApeBinarySensor *binarySensor = new ApeBinarySensor(this, pin);
+    input_pins_.push_back(binarySensor);
+    return binarySensor;
+  }
+  /*    sensor::Sensor *get_sensor(uint8_t pin)
     {
         ApeSensor *sensor = new ApeSensor(this, pin);
         analog_pins_.push_back(sensor);
         return sensor;
     }*/
 
-    void write_state(uint8_t pin, bool state, bool setup)
+  void write_state(uint8_t pin, bool state, bool setup = false)
+  {
+    if (this->configure_timeout_ != 0) return;
+#ifdef APE_DEBUG
+    ESP_LOGD(TAGape, "Writing %d to pin %d", state, pin);
+#endif
+    this->write_byte(state ? APE_CMD_WRITE_DIGITAL_HIGH : APE_CMD_WRITE_DIGITAL_LOW, pin);
+    if (setup)
     {
-        #ifdef APE_DEBUG
-        ESP_LOGI(TAGape, "Writing %d to pin %d", state, pin);
-        #endif
-        this->write_byte(state ? APE_CMD_WRITE_DIGITAL_HIGH : APE_CMD_WRITE_DIGITAL_LOW, pin);
-        if (setup) {
-            delay(20);
-            this->write_byte(APE_CMD_SETUP_PIN_OUTPUT, pin);
-        }
+      App.feed_wdt();
+      delay(20);
+      ESP_LOGI(TAGape, "Setup output pin %d", pin);
+      this->write_byte(APE_CMD_SETUP_PIN_OUTPUT, pin);
     }
+  }
 
-  protected:
-    bool initial_state_{true};
-    uint8_t read_buffer_[3]{0, 0, 0};
-    std::vector<ApeBinaryOutput *> output_pins_;
-    std::vector<ApeBinarySensor *> input_pins_;
-    //std::vector<ApeSensor *> analog_pins_;
+protected:
+  bool configure_{true};
+  bool initial_state_{true};
+  uint8_t read_buffer_[3]{0, 0, 0};
+  unsigned long configure_timeout_{5000};
+  std::vector<ApeBinaryOutput *> output_pins_;
+  std::vector<ApeBinarySensor *> input_pins_;
+  //std::vector<ApeSensor *> analog_pins_;
 };
 
 void ApeBinaryOutput::write_state(bool state)
 {
-    this->parent_->write_state(this->pin_, state, this->setup_);
-    this->setup_ = false;
+  this->state_ = state;
+  this->parent_->write_state(this->pin_, state);
 }
 
 /*
